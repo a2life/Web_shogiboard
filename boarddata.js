f986bd262be97afc2e6e238576b6ad2639f06dfb
10032268 <dadachi@us.yazaki.com>
2012-08-01 14:36:47 -0400

    clean up boarddata, javascript data structure

--- boarddata.js
+++ boarddata.js
@@ -6,24 +6,32 @@
  * To change this template use File | Settings | File Templates.
  * This is the storage for shobiboard moves. stored in the object in the array.
  */
-
+/* first declare the container.  In this case, object array by name of board*/
 var board=[];
+/* with help of modx script registering, I use push() function to add object element.  if there are three animated shogi
+* board in one page, there will be three push() string inside PHP side script
+* Combination
+* with each push, the content of the object is;
+* moves:[], index:0, history:[]
+* index will be always 0, history array will be always empty. moves array will have piece move information.
+* Original arrangement of the board is on server side PHP
+*/
 board.push( {
     /* p=pawn, l=lance, L=promoted lance, s=silver, S=promoted Silver, g=gold, r=rook, R=promoted Rook, b=bishop, B=promoted Bishop
      k=king 11=1a, 12=1b etc.,
      */
     marker:"lostworld", markerColor:"focus_trpt_g",
-/*refer to marker location and pattern "lost world" coordination is off the page
-//    onBoard:{G:['11l','21n','31s','41g','51k','61g','71s','81n','91l','22b','82r','13p','23p','33p','43p','53p','63p','73p','83p','93p'],
-//        S:['19l','29n','39s','49g','59k','69g','79s','89n','99l','28r','88b','17p','27p','37p','47p','57p','67p','77p','87p','97p']},
-// onBoard denotes pieces already on board. G array lists Gote side pieces. S array lists Sente side pieces.
-//    onHand:{ S:['g', 'g', 'l', 'p'],
-//        G:['b', 'l', 'l', 'p'] },
-// onhand denotes pieces on hand
+    /*refer to marker location and pattern "lost world" coordination is off the page
+     //    onBoard:{G:['11l','21n','31s','41g','51k','61g','71s','81n','91l','22b','82r','13p','23p','33p','43p','53p','63p','73p','83p','93p'],
+     //        S:['19l','29n','39s','49g','59k','69g','79s','89n','99l','28r','88b','17p','27p','37p','47p','57p','67p','77p','87p','97p']},
+     // onBoard denotes pieces already on board. G array lists Gote side pieces. S array lists Sente side pieces.
+     //    onHand:{ S:['g', 'g', 'l', 'p'],
+     //        G:['b', 'l', 'l', 'p'] },
+     // onhand denotes pieces on hand
 
-// moves is an array of moves. for first char, * is comment, s or g shows side.
-// second char is either - (move) or d for drop. cordinateion in /to/from order.
-*/
+     // moves is an array of moves. for first char, * is comment, s or g shows side.
+     // second char is either - (move) or d for drop. cordinateion in /to/from order.
+     */
     moves:[
         "*this is a comment that should go to comment line",
         "s-2627 *here, the sente moves a piece from 27 to 26",
@@ -43,21 +51,19 @@ board.push( {
     index:0,
     history:[]
     /*
-    * history array records the snapshot of the each move as a whole shogiboard and piece positions
-    *
+     * history array records the snapshot of the each move as a whole shogiboard and piece positions
+     *
      */
 });
 /*
-* to add another shogiboard, move is added as an array member. it will be retrieved by parser
-* with board[i].moves[index] fashion;
+ * to add another shogiboard, move is added as an array member. it will be retrieved by parser
+ * with board[i].moves[index] fashion;
  */
 board.push({
     moves:[
-    "*this is an essense of tsume problem",
+        "*this is an essense of tsume problem",
         "s+5216 *Sacrifice without capturing any piece!","g-5261","sd62s *Checkmate!","x"
     ],
     index:0,
-history:[]
+    history:[]
 });
-
-alert(board[0].moves[3]);
