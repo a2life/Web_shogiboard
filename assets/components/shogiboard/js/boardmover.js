1c698e72cde640b3ad59ea5dc582046c45b336a3
shared <dutch@adachis.info>
2012-12-12 00:21:15 -0500

    Push to shogishack production site,--Snapshot

--- assets/components/shogiboard/js/boardmover.js
+++ assets/components/shogiboard/js/boardmover.js
@@ -5,16 +5,8 @@
  * Time: 3:01 PM
  * To change this template use File | Settings | File Templates.
  */
-/**
- * Created with JetBrains WebStorm.
- * User: shared
- * Date: 6/3/12
- * Time: 10:51 PM
- * To change this template use File | Settings | File Templates.
- */
-
 /*jslint browser: true*/
-/*global  $,  boards, setupboard, */
+/*global  $,  boards,  */
 var boards = [];
 function komatopng(koma) {"use strict";
     //convert koma information and return image file name
@@ -215,53 +207,6 @@ function forwardOne(aBoard, self) {
     if (/[\-\+0-9pPlLnNsSgrRbB]+J/.test(aBoard.moves[aBoard.index])) {setupBranches(aBoard, self); }
 //after the move, if next line is a comment, then process it anyway.
 }
-
-function initializeBoard(j) { //i is a index to board data in boards array, as well as index to selected elem.
-//called from initializeBoards, iterate through the number of array element in boards[];
-    var i, cord, png, kpng, board = boards[j], target = $(".shogiBoard")[j],
-        boardbase = $(target).find('.boardbase'), sentemochigoma = $(target).find('.senteMochigoma'),
-        gotemochigoma = $(target).find('goteMochigoma');
-
-    //attach board marker to the boardbase. initially it is out of site
-
-    $('<img class="marker lostworld" id="marker" alt=""/>')
-        .addClass(board.marker).attr('src', board.filePathFocus  + board.markerImage).appendTo(boardbase);
-
-    /*  for (var index in board.Positions) {$('<img class="koma" alt=""/>')
-     .addClass(index).attr('src', pathname + board.Positions[index] + '.png').appendTo('#boardbase')};
-     */
-//create image elements with appropriate image and class information attached. also attach koma property with jquery.data()
-    for (i = 0; i < board.onBoard.S.length; i++) {
-
-        png = komatopng(board.onBoard.S[i].charAt(2));
-        kpng = komatopng((board.onBoard.S[i].charAt(2)).toLowerCase());
-        cord = board.onBoard.S[i].substr(0, 2);
-
-        $('<img alt=""/>')
-            .addClass(cordToClass(cord)).attr('src', board.filePathKoma + 'S' + png).attr('data-koma', kpng).appendTo(boardbase);
-    }
-    for (i = 0; i < board.onBoard.G.length; i++) {
-
-        png = komatopng(board.onBoard.G[i].charAt(2));
-        kpng = komatopng(board.onBoard.G[i].charAt(2).toLowerCase());
-        cord = board.onBoard.G[i].substr(0, 2);
-
-        $('<img alt=""/>')
-            .addClass(cordToClass(cord)).attr('src', board.filePathKoma + 'G' + png).attr('data-koma', kpng).appendTo(boardbase);
-    }
-
-    for (i = 0; i < board.onHand.S.length; i++) {
-        png = komatopng(board.onHand.S[i]);
-        $('<img class="" alt=""/>')
-            .attr('src', board.filePathKoma + 'S' + png).attr('data-koma', png).appendTo(sentemochigoma);
-    }
-
-    for (i = 0; i < board.onHand.G.length; i++) {
-        png = komatopng(board.onHand.G[i]);
-        $('<img class="" alt=""/>')
-            .attr('src', board.filePathKoma + 'G' + png).attr('data-koma', png).appendTo(gotemochigoma);
-    }
-}
 function setupButtons() {
     $('<input type="button">')
         .attr("class", "aButton")
@@ -287,14 +232,7 @@ function setupButtons() {
             );
 
 }
-function initializeBoards() {
-    var i;
-    for (i = 0; i < boards.length; i++) {
-        initializeBoard(i);
-    }
-}
+
 $(function () {
-//    setupboard();
-//    initializeBoards();
     setupButtons();
 });
